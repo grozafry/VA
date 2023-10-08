f81f0c5c7295331c0efca02955f67771ea9ff5ce
Suraj Bahuguna <surajbahuguna@Surajs-MacBook-Pro.local>
2023-10-08 23:28:40 +0530

    updated import_data script

--- search/management/commands/import_data.py
+++ search/management/commands/import_data.py
@@ -1,4 +1,3 @@
-# search/management/commands/import_data.py
 from django.core.management.base import BaseCommand
 import pandas as pd
 from search.models import DataStore
@@ -12,41 +11,41 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         file_path = kwargs['file_path']
         
-        # Read data from the text file using Pandas
-        df = pd.read_csv(file_path, sep='|')  # Update sep if needed
-
-        # Iterate through rows and save data to the database
-        for _, row in df.iterrows():
-            # print(row)
-            # print("line end")
-            model_instance = DataStore(
-                vin = row['vin'],
-                year = row['year'],
-                make = row['make'],
-                model = row['model'],
-                trim = row['trim'],
-                dealer_name = row['dealer_name'],
-                dealer_street = row['dealer_street'],
-                dealer_city = row['dealer_city'],
-                dealer_state = row['dealer_state'],
-                dealer_zip = row['dealer_zip'],
-                listing_price = row['listing_price'],
-                listing_mileage = row['listing_mileage'],
-                used = row['used'],
-                certified = row['certified'],
-                style = row['style'],
-                driven_wheels = row['driven_wheels'],
-                engine = row['engine'],
-                fuel_type = row['fuel_type'],
-                exterior_color = row['exterior_color'],
-                interior_color = row['interior_color'],
-                seller_website = row['seller_website'],
-                first_seen_date = row['first_seen_date'],
-                last_seen_date = row['last_seen_date'],
-                dealer_vdp_last_seen_date = row['dealer_vdp_last_seen_date'],
-                listing_status = row['listing_status'],
-
-            )
-            model_instance.save()
-
-        self.stdout.write(self.style.SUCCESS('Data imported successfully'))
+        try:
+            chunk_size = 20000 
+            for chunk in pd.read_csv(file_path, sep='|', chunksize=chunk_size):
+                records = []
+                for _, row in chunk.iterrows():
+                    record = DataStore(
+                        vin=row['vin'],
+                        year=row['year'],
+                        make=row['make'],
+                        model=row['model'],
+                        trim=row['trim'],
+                        dealer_name=row['dealer_name'],
+                        dealer_street=row['dealer_street'],
+                        dealer_city=row['dealer_city'],
+                        dealer_state=row['dealer_state'],
+                        dealer_zip=row['dealer_zip'],
+                        listing_price=row['listing_price'],
+                        listing_mileage=row['listing_mileage'],
+                        used=row['used'],
+                        certified=row['certified'],
+                        style=row['style'],
+                        driven_wheels=row['driven_wheels'],
+                        engine=row['engine'],
+                        fuel_type=row['fuel_type'],
+                        exterior_color=row['exterior_color'],
+                        interior_color=row['interior_color'],
+                        seller_website=row['seller_website'],
+                        first_seen_date=row['first_seen_date'],
+                        last_seen_date=row['last_seen_date'],
+                        dealer_vdp_last_seen_date=row['dealer_vdp_last_seen_date'],
+                        listing_status=row['listing_status'],
+                    )
+                    records.append(record)
+                DataStore.objects.bulk_create(records)
+                
+            self.stdout.write(self.style.SUCCESS('Data imported successfully'))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f'Error importing data: {str(e)}'))
